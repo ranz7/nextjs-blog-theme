d47217e038c7bdb5cccc9ca77b03c0f9b79f1b5e
ranz7 <rdkm.korepetycje@gmail.com>
2022-12-02 23:55:05 +0100

    First commit

--- pages/index.js
+++ pages/index.js
@@ -14,46 +14,43 @@ export default function Index({ posts, globalData }) {
 
     <div>
       <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
         <div>
           Ja postanowiłem stworzyć stronę przy pomocy Netlify.
         </div>
 
         <div>
           Najpierw trzeba było się zarejestrować na Netlify. Ja wybrałem opcję rejestracji przez GitHub.
-          <img src="images/1.png"></img>
+          <img src="https://ibb.co/sVWwBgv"></img>
         </div>
 
         <div>
           Później zostałem przeniesiony na dashboard, gdzie wybrałem opcję &quot;Start from a template&quot;.
-          <img src="images/2.png"></img>
+          <img src="https://ibb.co/GHwRMXt"></img>
         </div>
         
         <div>
           Następnie wybrałem Template &quot;Bejamas Next.js Blog Starter&quot; klikając w &quot;Source code&quot;
-          <img src="images/3.png"></img>
+          <img src="https://ibb.co/5FgtH5z"></img>
         </div>
 
         <div>
           Teraz sklonowałem template do mojego repozytorium na GitHubie klikając w &quot;GitHub&quot; na dole.
-          <img src="images/4.png"></img>
+          <img src="https://ibb.co/bFGCCYm"></img>
         </div>
 
         <div>
           To jest czas na wypełnienie prostych ustawień. Po wpisaniu wszystkich klikamy w &quot;Deploy&quot;.
-          <img src="images/5.png"></img>
+          <img src="https://ibb.co/xH6khTg"></img>
         </div>
 
         <div>
           Wystarczy poczekać aż strona sama się postawi.
-          <img src="images/6.png"></img>
+          <img src="https://ibb.co/0VJdXGQ"></img>
         </div>
 
         <div>
           Strona już działa. Można na nią wejść z dowolnej maszyny podpiętej do internetu.
-          <img src="images/7.png"></img>
+          <img src="https://ibb.co/fC0F3LV"></img>
         </div>
 
         <div>
