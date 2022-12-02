db476ae3f3fe5dcc11bc7ce27c61a058ba18985b
ranz7 <rdkm.korepetycje@gmail.com>
2022-12-03 00:34:38 +0100

    First commit

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,13 @@ import { getGlobalData } from '../utils/global-data';
 import SEO from '../components/SEO';
 
 
-import one from 'images/1.png';
-import two from 'images/2.png';
-import three from 'images/3.png';
-import four from 'images/4.png';
-import five from 'images/5.png';
-import six from 'images/6.png';
-import seven from 'images/7.png';
+import one from './images/1.png';
+import two from './images/2.png';
+import three from './images/3.png';
+import four from './images/4.png';
+import five from './images/5.png';
+import six from './images/6.png';
+import seven from './images/7.png';
 
 export default function Index({ posts, globalData }) {
   return (
