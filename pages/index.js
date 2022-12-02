ac65bf5f2ea8e028f87d9d8b2026b59f467b925a
ranz7 <rdkm.korepetycje@gmail.com>
2022-12-02 23:27:47 +0100

    First commit

--- pages/index.js
+++ pages/index.js
@@ -11,50 +11,49 @@ import SEO from '../components/SEO';
 export default function Index({ posts, globalData }) {
   return (
     <Layout>
-      <SEO title={globalData.name} description={globalData.blogTitle} />
-      <Header name={globalData.name} />
-      <main className="w-full">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">
-          {globalData.blogTitle}
-        </h1>
-        <ul className="w-full">
-          {posts.map((post) => (
-            <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-            >
-              <Link
-                as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
-                href={`/posts/[slug]`}
-              >
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.data.date && (
-                    <p className="uppercase mb-3 font-bold opacity-60">
-                      {post.data.date}
-                    </p>
-                  )}
-                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-                  {post.data.description && (
-                    <p className="mt-3 text-lg opacity-60">
-                      {post.data.description}
-                    </p>
-                  )}
-                  <ArrowIcon className="mt-4" />
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </main>
-      <Footer copyrightText={globalData.footerText} />
-      <GradientBackground
-        variant="large"
-        className="fixed top-20 opacity-40 dark:opacity-60"
-      />
-      <GradientBackground
-        variant="small"
-        className="absolute bottom-0 opacity-20 dark:opacity-10"
-      />
+      <div>
+        Ja postanowiłem stworzyć stronę przy pomocy Netlify.
+      </div>
+
+      <div>
+        Najpierw trzeba było się zarejestrować na Netlify. Ja wybrałem opcję rejestracji przez GitHub.
+        <img src="../../images/1.png"></img>
+      </div>
+
+      <div>
+        Później zostałem przeniesiony na dashboard, gdzie wybrałem opcję "Start from a template".
+        <img src="../../images/2.png"></img>
+      </div>
+      
+      <div>
+        Następnie wybrałem Template "Bejamas Next.js Blog Starter" klikając w "Source code"
+        <img src="../../images/3.png"></img>
+      </div>
+
+      <div>
+        Teraz sklonowałem template do mojego repozytorium na GitHubie klikając w "GitHub" na dole.
+        <img src="../../images/4.png"></img>
+      </div>
+
+      <div>
+        To jest czas na wypełnienie prostych ustawień. Po wpisaniu wszystkich klikamy w "Deploy".
+        <img src="../../images/5.png"></img>
+      </div>
+
+      <div>
+        Wystarczy poczekać aż strona sama się postawi.
+        <img src="../../images/6.png"></img>
+      </div>
+
+      <div>
+        Strona już działa. Można na nią wejść z dowolnej maszyny podpiętej do internetu.
+        <img src="../../images/7.png"></img>
+      </div>
+
+      <div>
+        Teraz wystarczy pushować zmiany do repozytorium na GitHubie, a zmiany są automatycznie dodawane na stronę.
+      </div>
+
     </Layout>
   );
 }
