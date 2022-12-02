eaefba2fdcb98e149da80735e04d6ab5c4f8b8b1
ranz7 <rdkm.korepetycje@gmail.com>
2022-12-02 23:42:35 +0100

    First commit

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,59 @@
-import Link from 'next/link';
-import { getPosts } from '../utils/mdx-utils';
+import Head from 'next/head'
 
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import Layout, { GradientBackground } from '../components/Layout';
-import ArrowIcon from '../components/ArrowIcon';
-import { getGlobalData } from '../utils/global-data';
-import SEO from '../components/SEO';
-
-export default function Index({ posts, globalData }) {
+export default function Index() {
   return (
-    <Layout>
-      <div>
-        Ja postanowiłem stworzyć stronę przy pomocy Netlify.
-      </div>
+    <div>
+      <Head>
+        <title>My app</title>
+      </Head>
 
-      <div>
-        Najpierw trzeba było się zarejestrować na Netlify. Ja wybrałem opcję rejestracji przez GitHub.
-        <img src="../images/1.png"></img>
-      </div>
+      <main>
+        <h1>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+        <div>
+          Ja postanowiłem stworzyć stronę przy pomocy Netlify.
+        </div>
 
-      <div>
-        Później zostałem przeniesiony na dashboard, gdzie wybrałem opcję &quot;Start from a template&quot;.
-        <img src="../images/2.png"></img>
-      </div>
-      
-      <div>
-        Następnie wybrałem Template &quot;Bejamas Next.js Blog Starter&quot; klikając w &quot;Source code&quot;
-        <img src="../images/3.png"></img>
-      </div>
+        <div>
+          Najpierw trzeba było się zarejestrować na Netlify. Ja wybrałem opcję rejestracji przez GitHub.
+          <img src="../images/1.png"></img>
+        </div>
 
-      <div>
-        Teraz sklonowałem template do mojego repozytorium na GitHubie klikając w &quot;GitHub&quot; na dole.
-        <img src="../images/4.png"></img>
-      </div>
+        <div>
+          Później zostałem przeniesiony na dashboard, gdzie wybrałem opcję &quot;Start from a template&quot;.
+          <img src="../images/2.png"></img>
+        </div>
+        
+        <div>
+          Następnie wybrałem Template &quot;Bejamas Next.js Blog Starter&quot; klikając w &quot;Source code&quot;
+          <img src="../images/3.png"></img>
+        </div>
 
-      <div>
-        To jest czas na wypełnienie prostych ustawień. Po wpisaniu wszystkich klikamy w &quot;Deploy&quot;.
-        <img src="../images/5.png"></img>
-      </div>
+        <div>
+          Teraz sklonowałem template do mojego repozytorium na GitHubie klikając w &quot;GitHub&quot; na dole.
+          <img src="../images/4.png"></img>
+        </div>
 
-      <div>
-        Wystarczy poczekać aż strona sama się postawi.
-        <img src="../images/6.png"></img>
-      </div>
+        <div>
+          To jest czas na wypełnienie prostych ustawień. Po wpisaniu wszystkich klikamy w &quot;Deploy&quot;.
+          <img src="../images/5.png"></img>
+        </div>
 
-      <div>
-        Strona już działa. Można na nią wejść z dowolnej maszyny podpiętej do internetu.
-        <img src="../images/7.png"></img>
-      </div>
+        <div>
+          Wystarczy poczekać aż strona sama się postawi.
+          <img src="../images/6.png"></img>
+        </div>
 
-      <div>
-        Teraz wystarczy pushować zmiany do repozytorium na GitHubie, a zmiany są automatycznie dodawane na stronę.
-      </div>
+        <div>
+          Strona już działa. Można na nią wejść z dowolnej maszyny podpiętej do internetu.
+          <img src="../images/7.png"></img>
+        </div>
 
-    </Layout>
+        <div>
+          Teraz wystarczy pushować zmiany do repozytorium na GitHubie, a zmiany są automatycznie dodawane na stronę.
+        </div>
+      </main>
+    </div>
   );
-}
-
-export function getStaticProps() {
-  const posts = getPosts();
-  const globalData = getGlobalData();
-
-  return { props: { posts, globalData } };
 }
