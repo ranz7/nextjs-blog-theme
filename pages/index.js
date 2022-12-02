1814e7cc828f148dca1f54fb00d621c069e2d69b
ranz7 <rdkm.korepetycje@gmail.com>
2022-12-02 23:49:07 +0100

    First commit

--- pages/index.js
+++ pages/index.js
@@ -23,37 +23,37 @@ export default function Index({ posts, globalData }) {
 
         <div>
           Najpierw trzeba było się zarejestrować na Netlify. Ja wybrałem opcję rejestracji przez GitHub.
-          <img src="../images/1.png"></img>
+          <img src="images/1.png"></img>
         </div>
 
         <div>
           Później zostałem przeniesiony na dashboard, gdzie wybrałem opcję &quot;Start from a template&quot;.
-          <img src="../images/2.png"></img>
+          <img src="images/2.png"></img>
         </div>
         
         <div>
           Następnie wybrałem Template &quot;Bejamas Next.js Blog Starter&quot; klikając w &quot;Source code&quot;
-          <img src="../images/3.png"></img>
+          <img src="images/3.png"></img>
         </div>
 
         <div>
           Teraz sklonowałem template do mojego repozytorium na GitHubie klikając w &quot;GitHub&quot; na dole.
-          <img src="../images/4.png"></img>
+          <img src="images/4.png"></img>
         </div>
 
         <div>
           To jest czas na wypełnienie prostych ustawień. Po wpisaniu wszystkich klikamy w &quot;Deploy&quot;.
-          <img src="../images/5.png"></img>
+          <img src="images/5.png"></img>
         </div>
 
         <div>
           Wystarczy poczekać aż strona sama się postawi.
-          <img src="../images/6.png"></img>
+          <img src="images/6.png"></img>
         </div>
 
         <div>
           Strona już działa. Można na nią wejść z dowolnej maszyny podpiętej do internetu.
-          <img src="../images/7.png"></img>
+          <img src="images/7.png"></img>
         </div>
 
         <div>
